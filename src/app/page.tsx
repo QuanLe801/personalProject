f40c5e768e0f6300fa2b7dd1c075a6a98b66c6ea
quanlm1 <quanlm1@synodus.com>
2023-05-23 14:25:54 +0700

    setup-base Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div>
-      <div>Hello</div>
+      <div className={styles.header}>
+        Chào mừng đến với môi trường <span className="d-block">Vlearning</span>
+      </div>
     </div>
   );
 }
